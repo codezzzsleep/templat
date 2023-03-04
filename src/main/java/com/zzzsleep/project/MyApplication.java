d634aef9f6fd539b7be84fbeb74891a628bdbea2
codezzzsleep <codezzzsleep@163.com>
2023-03-04 15:34:31 +0800

    init

---
+++ src/main/java/com/zzzsleep/project/MyApplication.java
@@ -0,0 +1,14 @@
+package com.zzzsleep.project;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.zzzsleep.project.mapper")
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
+
